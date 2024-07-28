a0ad3306673cb8c846670e829f87a5c1e5eac89b
MGomez97 <manuelgomez19010@gmail.com>
2024-07-28 14:18:42 -0600

    Create ImplementacionOperaciones.cs

---
+++ Ejercicio Herencia/Ejercicio Herencia/ImplementacionOperaciones.cs
@@ -0,0 +1,14 @@
+﻿public class ImplementacionOperaciones : Operaciones
+{
+    public void Imprimir()
+    {
+        double areaRectangulo = CalcularAreaRectangulo(5, 10);
+        double volumenCubo = CalcularVolumenCubo(3);
+        double longitudCircunferencia = CalcularLongitudCircunferencia(7);
+
+        Console.WriteLine("Resultados de las Operaciones:");
+        Console.WriteLine($"Área del Rectángulo: {areaRectangulo}");
+        Console.WriteLine($"Volumen del Cubo: {volumenCubo}");
+        Console.WriteLine($"Longitud de la Circunferencia: {longitudCircunferencia}");
+    }
+}
