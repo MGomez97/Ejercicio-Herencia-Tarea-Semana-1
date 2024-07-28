d740efce429517c97612dba703029401cc8ea1ef
MGomez97 <manuelgomez19010@gmail.com>
2024-07-28 14:15:03 -0600

    Create Operaciones.cs

---
+++ Ejercicio Herencia/Ejercicio Herencia/Operaciones.cs
@@ -0,0 +1,17 @@
+﻿public class Operaciones
+{
+    public double CalcularAreaRectangulo(double largo, double ancho)
+    {
+        return largo * ancho;
+    }
+
+    public double CalcularVolumenCubo(double lado)
+    {
+        return lado * lado * lado;
+    }
+
+    public double CalcularLongitudCircunferencia(double radio)
+    {
+        return 2 * Math.PI * radio;
+    }
+}
